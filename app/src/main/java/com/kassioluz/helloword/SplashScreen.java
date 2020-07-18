d90c35e487107f607ab63c7e36b4f78ebdbb1a24
KassioVieira <kassio.vieira7@gmail.com>
2020-07-18 13:58:09 -0300

    trabalho prático android

--- app/src/main/java/com/kassioluz/helloword/SplashScreen.java
+++ app/src/main/java/com/kassioluz/helloword/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.kassioluz.helloword;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -9,23 +11,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Handler handle = new Handler();
-        handle.postDelayed(new Runnable() {
-            @Override public void run() {
-               showLogin();
-            }
-        }, 4000);
+        if(isFirstTime()){
+            saveFirstTime();
+
+            Handler handle = new Handler();
+            handle.postDelayed(new Runnable() {
+                @Override public void run() {
+                    showMain();
+                }
+            }, 4000);
+
+        }else {
+            showMain();
+        }
     }
 
-    private void showLogin() {
+    private void showMain() {
         Intent intent = new Intent(SplashScreen.this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+
+    private boolean isFirstTime(){
+
+        sharedPreferences = getSharedPreferences("first", Context.MODE_PRIVATE);
+        boolean result = sharedPreferences.getBoolean("first",true);
+
+        return result;
+    }
+
+    private void saveFirstTime(){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("first", false);
+        editor.commit();
     }
 
 }
